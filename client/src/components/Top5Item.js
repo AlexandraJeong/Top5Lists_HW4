e3474272686b84ec77c6c3edf74eb5a878eebf14
Alexandra Jeong <alexandra.jeong@stonybrook.edu>
2021-11-06 15:44:10 -0400

    editting items

--- client/src/components/Top5Item.js
+++ client/src/components/Top5Item.js
@@ -15,7 +15,7 @@ function Top5Item(props) {
     const { store } = useContext(GlobalStoreContext);
     const [editActive, setEditActive] = useState(false);
     const [draggedTo, setDraggedTo] = useState(0);
-
+    const [ text, setText ] = useState("");
     function handleDragStart(event, targetId) {
         event.dataTransfer.setData("item", targetId);
     }
@@ -48,32 +48,49 @@ function Top5Item(props) {
         store.addMoveItemTransaction(sourceId, targetId);
     }
 
+    function handleKeyPress(event) {
+        if (event.code === "Enter") {
+            let id = parseInt(event.target.id.substring("item-".length))-1;
+            store.addUpdateItemTransaction(index,text);
+            setEditActive(false);
+        }// index, newtext
+    }
+
+    function handleUpdateText(event) {
+        setText(event.target.value);
+    }
+
     let { index } = props;
 
     let itemClass = "top5-item";
     if (draggedTo) {
         itemClass = "top5-item-dragged-to";
     }
-
-    return (
+    function handleClick(event) {
+        console.log("wow");//scrap
+        event.stopPropagation();
+        setEditActive(true);
+    }
+    if (!editActive) {
+        return (
             <ListItem
-                id={'item-' + (index+1)}
+                id={'item-' + (index + 1)}
                 key={props.key}
                 className={itemClass}
                 onDragStart={(event) => {
-                    handleDragStart(event, (index+1))
+                    handleDragStart(event, (index + 1))
                 }}
                 onDragOver={(event) => {
-                    handleDragOver(event, (index+1))
+                    handleDragOver(event, (index + 1))
                 }}
                 onDragEnter={(event) => {
-                    handleDragEnter(event, (index+1))
+                    handleDragEnter(event, (index + 1))
                 }}
                 onDragLeave={(event) => {
-                    handleDragLeave(event, (index+1))
+                    handleDragLeave(event, (index + 1))
                 }}
                 onDrop={(event) => {
-                    handleDrop(event, (index+1))
+                    handleDrop(event, (index + 1))
                 }}
                 draggable="true"
                 sx={{ display: 'flex', p: 1 }}
@@ -82,14 +99,28 @@ function Top5Item(props) {
                     width: '100%'
                 }}
             >
-            <Box sx={{ p: 1 }}>
-                <IconButton aria-label='edit'>
-                    <EditIcon style={{fontSize:'48pt'}}  />
-                </IconButton>
-            </Box>
+                <Box sx={{ p: 1 }}>
+                    <IconButton aria-label='edit' onClick={handleClick}>
+                        <EditIcon style={{ fontSize: '48pt' }} />
+                    </IconButton>
+                </Box>
                 <Box sx={{ p: 1, flexGrow: 1 }}>{props.text}</Box>
             </ListItem>
-    )
+        );
+    } else {
+        return <input
+            sx={{ display: 'flex', p: 1 }}
+            style={{
+                fontSize: '48pt',
+                width: '100%'
+            }}
+            id={"item-" + (index + 1)}
+            type='text'
+            onKeyPress={handleKeyPress}
+            onChange={handleUpdateText}
+            defaultValue={props.text}
+        />;
+    }
 }
 
 export default Top5Item;
